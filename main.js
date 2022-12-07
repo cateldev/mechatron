60ded6b14bf7baf733fcb295edcd91b3d677b7a8
catelmatt <catelmatt@hotmail.com>
2022-12-06 23:53:16 -0300

    Started scripting

---
+++ main.js
@@ -0,0 +1,6 @@
+const element = document.querySelector("calculate");
+const result = document.querySelector(".result");
+
+element.addEventListener("click", (event) => {
+  result.innerHTML = "Clicked";
+});
