5e65082c803b40cfc7f95c48b1e2635cd1ef0f4f
catelmatt <catelmatt@hotmail.com>
2022-12-09 01:49:21 -0300

    Started dataHandler

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const element = document.querySelector("calculate");
-const result = document.querySelector(".result");
+const subtract = document.querySelector("#subtract");
+const plus = document.querySelector("#plus");
+const arm = document.querySelector("#arm");
 
-element.addEventListener("click", (event) => {
-  result.innerHTML = "Clicked";
+const control = document.querySelectorAll(".control-ajust");
+
+control.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    dataHandler(event.target.textContent);
+  });
 });
+
+function dataHandler(operator) {
+  if (operator === "-") {
+    arm.value = parseInt(arm.value) - 1;
+  } else {
+    arm.value = parseInt(arm.value) + 1;
+  }
+}
+
+//How to remove an item from an array
+var list = ["Orange", "Red", "White", "Yellow", "Pink"];
+list.splice(1, 1);
+console.log(list);
